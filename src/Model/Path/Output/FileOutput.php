c7f9b136a0c509b56ba6a8fb7efde1103c481cfa
sascha pogadaev <76207138+apogadaev@users.noreply.github.com>
2023-05-12 15:46:53 +0200

    Add CSV text file output (#81)

--- src/Model/Path/Output/FileOutput.php
+++ src/Model/Path/Output/FileOutput.php
@@ -45,6 +45,11 @@ final class FileOutput implements Output
         return new self('application/zip');
     }
 
+    public static function forCsvText(): self
+    {
+        return new self('text/csv');
+    }
+
     /**
      * {@inheritDoc}
      */
